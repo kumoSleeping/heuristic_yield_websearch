7b694ffb36cffb2d1e5f08544a882fe93d5399c8
kumoSleeping <zjr2992@outlook.com>
2026-03-26 23:13:49 +0800

    release 0.0.9

--- entari_plugin_hyw/__init__.py
+++ entari_plugin_hyw/__init__.py
@@ -1,5 +1,5 @@
 """Entari plugin package entrypoint."""
 
-import core.entari_plugin_hyw as _impl  # entari: plugin
+import core.entari_plugin_hyw as _impl  # entari: package
 
 __version__ = _impl.__version__
